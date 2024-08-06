ff8296abbd403eb81c437c0cf1343468d2a83085
romiyadangol <dangolromiya@gmail.com>
2024-08-06 12:55:30 +0545

    Added validation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  http_basic_authenticate_with name: "romi", password: "secret", only: :destroy
     def create
       @article = Article.find(params[:article_id])
       puts @article
